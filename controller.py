c53775dd6e3061967c47fed4ab0f5b8f856c5f72
GyroW <wouter.lemoine.658@gmail.com>
2017-03-17 17:24:51 +0100

    Debugging, the input thing made it work, but for some reason doing k = input() print(k) is not acceptable

--- controller.py
+++ controller.py
@@ -260,8 +260,8 @@ def updatevisual():
     VPP2.grid(row=1,column=2, pady=(0,ypadding))
     VPP3.grid(row=3,column=0)
     VPP4.grid(row=3,column=2)
-    thisrandomvariablenameijustcameupwith = input()
-    print(thisrandomvariablenameijustcameupwith)
+    input("Waiting for the weird visual thing:")
+   
 
 
 
